bbf7f5ca1ddabd864ac97f54c9f4b1e160087214
NDOPING WILSON FOMBASOH <wilsonndoping@gmail.com>
2025-08-01 04:46:33 +0100

    aldfjla

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:9000";
+const API_BASE_URL = "https://trackyourexpensesbackend.onrender.com/";
 
 class ApiService {
   async request(endpoint, options = {}) {
